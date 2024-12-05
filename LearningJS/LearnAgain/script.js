2523edf205b3f036c10527c45fe9079beb753d7e
Amos Samwel <professorprofessor252@gmail.com>
2024-12-05 21:58:27 +0300

    I am Learning  JS

---
+++ LearningJS/LearnAgain/script.js
@@ -0,0 +1,46 @@
+/*console.log(`Hello World!`);
+console.log(`My name is Amos Daniel Samwel`);
+console.log(`I am Learning Javascript`);
+console.log(`It is so intimidating, but I hope I will catch up very Fast`);
+window.alert(`This is an Alert`);
+window.alert(`I like Pizza`);
+window.alert(`Fuck you`);*/
+
+/*document.getElementById(`myH4`).textContent=`Hello World`;
+document.getElementById(`myP1`).textContent=`I Like Pizza`;
+let x = 100;
+let y = 220;
+let age = 25;
+let price = 10.99;
+let gpa = 4.5;
+let firstName = "Amos";
+let secondName = "Daniel";
+let lastName = "Samwel";
+console.log(x);
+console.log(x+y);
+console.log(age);
+console.log(price);
+console.log(gpa);
+
+//Using Place Holders 
+console.log(`You are ${age} years Old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is ${gpa}`);
+
+//Outiputting the Data types
+console.log(typeof age);
+console.log(age);
+console.log(`Are your names ${firstName}`+" "+`${secondName}`+" "+`${lastName}?`);*/
+
+/*let online = true;
+let ofline = false;
+console.log(`Amos is Online: ${online}`);
+console.log(`Gilly is Online: ${ofline}`);*/
+
+let fullName = "Amos samwel";
+let age = 25;
+let student = false;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
